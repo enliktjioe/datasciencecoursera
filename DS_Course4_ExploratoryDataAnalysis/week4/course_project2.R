126d856278d564fec9f02269655ef0c5ef7f1492
Enlik Tjioe <enlik.stefanus@gmail.com>
2019-01-19 22:05:26 +0700

    Starting Course Project 2 (week 4 - EDA)

---
+++ DS_Course4_ExploratoryDataAnalysis/week4/course_project2.R
@@ -0,0 +1,4 @@
+setwd("/Users/enlik/GitRepo/datasciencecoursera/DS_Course4_ExploratoryDataAnalysis/week4")
+
+NEI <- readRDS("exdata_data_NEI_data/summarySCC_PM25.rds")
+SCC <- readRDS("exdata_data_NEI_data/Source_Classification_Code.rds")
